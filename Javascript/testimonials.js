a8c63c4c14ff66a954b42d2b412d8a728d3da48d
Ginette Tonoukouin <tonoukouinginette288@gmail.com>
2023-03-25 17:41:12 +0100

    Website hotel

---
+++ Javascript/testimonials.js
@@ -0,0 +1,10 @@
+let links = document.querySelectorAll('.image-link');
+for (let link of links) {
+  link.addEventListener('click', function() {
+    for (let l of links) {
+      l.classList.remove('active');
+    }
+    this.classList.add('active');
+  });
+}
+AOS.init();
